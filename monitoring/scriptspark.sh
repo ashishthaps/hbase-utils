d246063e1543f084f762d98de57e331a3182ebcc
Ashish Thapliyal <ashishthaps@hotmail.com>
2016-10-30 23:11:29 -0700

    latest update .75

--- monitoring/scriptspark.sh
+++ monitoring/scriptspark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_GA_v1.2.0-25/omsagent-1.2.0-25.universal.x64.sh -O /tmp/omsagent.x64.sh
+wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_GA_v1.2.0-25/omsagent-1.2.0-75.universal.x64.sh -O /tmp/omsagent.x64.sh
 sudo sh /tmp/omsagent.x64.sh --upgrade
 if [[ $HOSTNAME == hn* ]];
 then
