f289170aaf3f4d7ebabb2af8461bbe8d0ea3e06e
Ashish Thapliyal <ashishthaps@hotmail.com>
2017-01-30 20:48:03 -0800

    Update hivescript.sh

--- monitoring/hivescript.sh
+++ monitoring/hivescript.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-
-wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_GA_v1.2.0-25/omsagent-1.2.0-25.universal.x64.sh -O /tmp/omsagent.x64.sh
+wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent-201610-v1.2.0-148/omsagent-1.2.0-148.universal.x64.sh -O /tmp/omsagent.x64.sh
 sudo sh /tmp/omsagent.x64.sh --upgrade
 if [[ $HOSTNAME == hn* ]];
 then
-  sudo wget https://raw.githubusercontent.com/ashishthaps/hbase-utils/master/monitoring/spark.headnode.conf -O /etc/opt/microsoft/omsagent/conf/omsagent.d/spark.headnode.conf
   sudo wget https://raw.githubusercontent.com/Azure/hbase-utils/master/monitoring/yarn.headnode.conf -O /etc/opt/microsoft/omsagent/conf/omsagent.d/yarn.headnode.conf
 sudo wget https://raw.githubusercontent.com/ashishthaps/hbase-utils/master/monitoring/hive.headnode.conf -O /etc/opt/microsoft/omsagent/conf/omsagent.d/hive.headnode.conf
 else
-  sudo wget https://raw.githubusercontent.com/ashishthaps/hbase-utils/master/monitoring/spark.workernode.conf -O /etc/opt/microsoft/omsagent/conf/omsagent.d/spark.workernode.conf
   sudo wget https://raw.githubusercontent.com/Azure/hbase-utils/master/monitoring/yarn.workernode.conf -O /etc/opt/microsoft/omsagent/conf/omsagent.d/yarn.workernode.conf
 
 fi
